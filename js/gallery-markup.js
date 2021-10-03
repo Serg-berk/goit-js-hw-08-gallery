79170540654007a657a4634c06da450677f9dd65
Sergii Griazin <gig_troy@ukr.net>
2021-10-03 17:01:48 +0300

    hw-08

---
+++ js/gallery-markup.js
@@ -0,0 +1,46 @@
+import gallery from './gallery-Items.js';
+import refs from './refs.js';
+const { galleryImgs, modal, modalImg, closeModalBtn, modalOverlay } = refs;
+
+
+const gallerysMarkup = getMarkup(gallery)
+galleryImgs.insertAdjacentHTML('beforeend', gallerysMarkup);
+
+function getMarkup(gallery) {
+  return gallery.map(({ preview, original, description })=>
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/> 
+      </a>
+    </li>`,
+).join('');
+}
+
+galleryImgs.addEventListener('click', onImgClick);
+
+function onImgClick(e) {
+  e.preventDefault()
+ if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
+  modal.classList.add("is-open");
+  modalImg.src = e.target.dataset.source
+  modalImg.alt = e.target.alt
+}
+
+modalOverlay.addEventListener("click", modalClose);
+closeModalBtn.addEventListener("click", modalClose);
+function modalClose() {
+  modal.classList.remove("is-open");
+}
+ 
+window.addEventListener('keydown', onEscKeyPress)
+
+function onEscKeyPress(event) {
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = event.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    modal.classList.remove("is-open");
+  }
+}
---
+++ js/refs.js
@@ -0,0 +1,7 @@
+export default {
+  galleryImgs: document.querySelector('.js-gallery'),
+  modal: document.querySelector(".js-lightbox"),
+  modalImg: document.querySelector(".lightbox__image"),
+  closeModalBtn: document.querySelector('[data-action="close-lightbox"]'),
+  modalOverlay: document.querySelector(".lightbox__overlay"),
+};
